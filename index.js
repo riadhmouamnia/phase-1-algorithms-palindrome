28df07e0ca6677a187fdd6b15bfa92fa693c8368
Riadh Mouamnia <riadh.mouamnia@gmail.com>
2023-08-23 14:06:34 +0100

    Completed assignment

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  let reverseIndex = word.length - 1;
+  let result;
+  for (let i in word) {
+    if (word[i] === word[reverseIndex]) {
+      result = true;
+    } else {
+      result = false;
+    }
+    reverseIndex--;
+  }
+  return result;
 }
 
 /* 
   Add your pseudocode here
+  1- Create a tracing reverse index starting from the ending index of that string
+  2- Create a new result variable that will hold the result of the comparison
+  3- Loop for each letter in the word in the index and check if it's equal to the word in the reverse index 
+  and decrement the reverse index in each iteration of the loop
+  4- Set the result to true if it's equal to the word in the reverse index and false if it's not equal
+  5- Return the result
 */
 
 /*
